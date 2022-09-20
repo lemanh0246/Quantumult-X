6ea15648fbeab2010ce016b425af5c16c6437795
lemanh0246 <112248199+lemanh0246@users.noreply.github.com>
2022-09-20 21:10:55 +0700

    Update nicegram.js

--- nicegram.js
+++ nicegram.js
@@ -1,1 +1,1 @@
-{"settings":{"sync_chats":true,"custom_notifications":false,"max_pinned_chats":5},"reasons":["porn","copyright"],"allowed":[],"restricted":[845385857,699361853],"premium":false}
+{"settings":{"sync_chats":true,"custom_notifications":false,"max_pinned_chats":5},"reasons":["porn","copyright"],"allowed":[],"restricted":[845385857,699361853],"premiumAccess":false}
